5ee64119760680d4abc01a4049226660c0189004
Siigmaf <faby.tbe@gmail.com>
2023-04-13 11:45:10 -0300

    lógia imc

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ImcCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("Digite o seu peso (em kg): ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Peso lido do console: " + peso);
+
+            Console.Write("Digite a sua altura (em metros): ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Altura lida do console: " + altura);
+
+            double imc = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: " + imc.ToString("0.00"));
+        }
+    }
+}
+
